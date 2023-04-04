95a340607fbf58d3348c9c06ec6337200c642871
Liz Fosdick <lizfosdick@gmail.com>
2023-04-03 20:36:47 -0700

    Exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,23 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+class Store
+  has_many :employees
+end
+
+class Employee
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Taylor", last_name: "Swift", hourly_rate: 100)
+@store1.employees.create(first_name: "Chrissy", last_name: "Teigen", hourly_rate: 70)
+@store1.employees.create(first_name: "Nicole", last_name: "Byer", hourly_rate: 90)
+@store1.employees.create(first_name: "Lizzo", last_name: "Lizzoslastname", hourly_rate: 80)
+@store1.employees.create(first_name: "Harry", last_name: "Styles", hourly_rate: 75)
+@store2.employees.create(first_name: "Lisa", last_name: "Vanderpump", hourly_rate: 110)
+@store2.employees.create(first_name: "Kyle", last_name: "Richards", hourly_rate: 110)
+@store2.employees.create(first_name: "Gizelle", last_name: "Bryant", hourly_rate: 110)
+@store2.employees.create(first_name: "Luann", last_name: "Delesseps", hourly_rate: 110)
+@store2.employees.create(first_name: "Sonja", last_name: "Morgan", hourly_rate: 110)
+
