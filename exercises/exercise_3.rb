46dc155af15c233a4f7f5f6728674dd770e7ed86
Liz Fosdick <lizfosdick@gmail.com>
2023-04-03 20:11:45 -0700

    Exercise 4

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -9,9 +9,3 @@ puts "----------"
 @store3.destroy
 puts Store.count
 
-
-=begin
-1. Load the third store (into `@store3`) as you did the other two before.
-2. Using Active Record's `destroy` method, delete the store from the database.
-3. Verify that the store has been deleted by again outputting (`puts`ing) the `count` (as you did in Exercise 1.)
-=end
